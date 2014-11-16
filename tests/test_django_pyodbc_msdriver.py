37d47937f7e023756257f3103302656e66626eae
Paul <paultax@gmail.com>
2014-11-16 20:04:51 +0100

    11

---
+++ tests/test_django_pyodbc_msdriver.py
@@ -0,0 +1,36 @@
+import os
+
+PASSWORD = os.environ.get('MSSQL_PASSWORD', 'myp@ssword')
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django_pyodbc",
+        'HOST': "tcp:localhost,49544",
+        'USER': "sa",
+        'PASSWORD': PASSWORD,
+        'NAME': "django_odbc_test",
+        'OPTIONS': {
+            'encoding': 'utf-8',
+            'driver': "SQL Server Native Client 11.0",
+            'autocommit': True,
+        },
+    },
+    'other': {
+        'ENGINE': "django_pyodbc",
+        'HOST': "tcp:localhost,49544",
+        'USER': "sa",
+        'PASSWORD': PASSWORD,
+        'NAME': "otherdb",
+        'OPTIONS': {
+            'encoding': 'utf-8',
+            'driver': "SQL Server Native Client 11.0",
+            'autocommit': True,
+        },
+    },
+}
+SECRET_KEY = "django_tests_secret_key"
+
+# Use a fast hasher to speed up tests.
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
